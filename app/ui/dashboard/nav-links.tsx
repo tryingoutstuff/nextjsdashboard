0fc144b6cfbe8f5774fb1f43590152643fa382b1
tryingoutstuff <130311117+tryingoutstuff@users.noreply.github.com>
2024-04-23 23:04:27 -0700

    Update nav-links.tsx

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -9,8 +9,15 @@ import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import clsx from 'clsx';
  
-// ...test code...
- 
+const links = [
+  { name: 'Home', href: '/dashboard', icon: HomeIcon },
+  {
+    name: 'Invoices',
+    href: '/dashboard/invoices',
+    icon: DocumentDuplicateIcon,
+  },
+  { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
+];
 export default function NavLinks() {
   const pathname = usePathname();
  
